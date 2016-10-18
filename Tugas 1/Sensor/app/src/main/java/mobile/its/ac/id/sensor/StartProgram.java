dc2a4df372860f80e40535153a11dc3d03dbe890
hanifsudira <sudirahanif@gmail.com>
2016-10-18 07:51:56 +0700

    progress

--- Tugas 1/Sensor/app/src/main/java/mobile/its/ac/id/sensor/StartProgram.java
+++ Tugas 1/Sensor/app/src/main/java/mobile/its/ac/id/sensor/StartProgram.java
@@ -1,13 +1,21 @@
 package mobile.its.ac.id.sensor;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
 import com.opencsv.CSVWriter;
 
 import org.w3c.dom.Text;
@@ -26,13 +34,13 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
-public class StartProgram extends AppCompatActivity{
-    private TextView type,existlight,valuelight,record,x,y,z,gx,gy,gz;
-    private Sensor lightSensor,accelerometer,gyrometer;
+public class StartProgram extends AppCompatActivity {
+    private TextView type, existlight, valuelight, record, x, y, z, gx, gy, gz, speed;
+    private Sensor lightSensor, accelerometer, gyrometer;
     private float myvaluelight = -1;
     private SensorManager sensorManager;
     private SensorEventListener sensorEventListener;
-    private boolean isrecord,isrecordGyro = false;
+    private boolean isrecord, isrecordGyro = false;
     private String datefile = "", datefilegyro = "", nowtime;
     private List<String[]> dataAcce = new ArrayList<String[]>();
     private List<String[]> dataGyro = new ArrayList<String[]>();
@@ -40,60 +48,61 @@ public class StartProgram extends AppCompatActivity{
     private float[][] dataTest = new float[50][3];
     private int counter = 0;
 
-    public void writeTocsv() throws IOException{
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    public void writeTocsv() throws IOException {
         String basedir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
-        String filepath = basedir+File.separator+datefile+"_Acce.csv";
+        String filepath = basedir + File.separator + datefile + "_Acce.csv";
         File file = new File(filepath);
         CSVWriter writer;
-        if(file.exists() && !file.isDirectory()){
+        if (file.exists() && !file.isDirectory()) {
             FileWriter fileWriter = new FileWriter(filepath);
             writer = new CSVWriter(fileWriter);
-        }
-        else {
+        } else {
             writer = new CSVWriter(new FileWriter(filepath));
         }
         writer.writeAll(dataAcce);
         writer.close();
     }
 
-    public void writeTocsvGyro() throws  IOException{
+    public void writeTocsvGyro() throws IOException {
         String basedir = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
-        String filepath = basedir+File.separator+datefilegyro+"_Gyro.csv";
+        String filepath = basedir + File.separator + datefilegyro + "_Gyro.csv";
         File file = new File(filepath);
         CSVWriter writer;
-        if(file.exists() && !file.isDirectory()){
+        if (file.exists() && !file.isDirectory()) {
             FileWriter fileWriter = new FileWriter(filepath);
             writer = new CSVWriter(fileWriter);
-        }
-        else {
+        } else {
             writer = new CSVWriter(new FileWriter(filepath));
         }
         writer.writeAll(dataGyro);
         writer.close();
     }
 
-    public void myKnn(){
-        File folder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),"datasensor");
-        File file = new File(folder,"motor.csv");
-        try{
+    public void myKnn() {
+        File folder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "datasensor");
+        File file = new File(folder, "motor.csv");
+        try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
             String line;
             float[][] buffer = new float[50][3];
-            int a=0,b=0;
+            int a = 0, b = 0;
 
-            while((line = bufferedReader.readLine())!=null && a < 5000){
-                String[] temp = line.replace("\"","").split(",");
-                buffer[a%50][0] = Float.parseFloat(temp[0]);
-                buffer[a%50][1] = Float.parseFloat(temp[1]);
-                buffer[a%50][2] = Float.parseFloat(temp[2]);
+            while ((line = bufferedReader.readLine()) != null && a < 5000) {
+                String[] temp = line.replace("\"", "").split(",");
+                buffer[a % 50][0] = Float.parseFloat(temp[0]);
+                buffer[a % 50][1] = Float.parseFloat(temp[1]);
+                buffer[a % 50][2] = Float.parseFloat(temp[2]);
                 a++;
 
-                if(a%50==0){
-                    float[] temp2 = {0,0,0};
-                    for(int i=0;i<50;i++){
-                        temp2[0] = buffer[i][0]/50;
-                        temp2[1] = buffer[i][1]/50;
-                        temp2[2] = buffer[i][2]/50;
+                if (a % 50 == 0) {
+                    float[] temp2 = {0, 0, 0};
+                    for (int i = 0; i < 50; i++) {
+                        temp2[0] = buffer[i][0] / 50;
+                        temp2[1] = buffer[i][1] / 50;
+                        temp2[2] = buffer[i][2] / 50;
                     }
                     dataTraining[b][0] = temp2[0];
                     dataTraining[b][1] = temp2[1];
@@ -101,33 +110,32 @@ public class StartProgram extends AppCompatActivity{
                     b++;
                 }
             }
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        File file2 = new File(folder,"Jalan.csv");
-        try{
+        File file2 = new File(folder, "Jalan.csv");
+        try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file2));
             String line;
             float[][] buffer = new float[50][3];
-            int a=0,b=100;
+            int a = 0, b = 100;
 
-            while((line = bufferedReader.readLine())!=null && a < 5000){
-                String[] temp = line.replace("\"","").split(",");
-                buffer[a%50][0] = Float.parseFloat(temp[0]);
-                buffer[a%50][1] = Float.parseFloat(temp[1]);
-                buffer[a%50][2] = Float.parseFloat(temp[2]);
+            while ((line = bufferedReader.readLine()) != null && a < 5000) {
+                String[] temp = line.replace("\"", "").split(",");
+                buffer[a % 50][0] = Float.parseFloat(temp[0]);
+                buffer[a % 50][1] = Float.parseFloat(temp[1]);
+                buffer[a % 50][2] = Float.parseFloat(temp[2]);
                 a++;
 
-                if(a%50==0){
-                    float[] temp2 = {0,0,0};
-                    for(int i=0;i<50;i++){
-                        temp2[0] = buffer[i][0]/50;
-                        temp2[1] = buffer[i][1]/50;
-                        temp2[2] = buffer[i][2]/50;
+                if (a % 50 == 0) {
+                    float[] temp2 = {0, 0, 0};
+                    for (int i = 0; i < 50; i++) {
+                        temp2[0] = buffer[i][0] / 50;
+                        temp2[1] = buffer[i][1] / 50;
+                        temp2[2] = buffer[i][2] / 50;
                     }
                     dataTraining[b][0] = temp2[0];
                     dataTraining[b][1] = temp2[1];
@@ -135,68 +143,70 @@ public class StartProgram extends AppCompatActivity{
                     b++;
                 }
             }
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_program);
         myKnn();
-        existlight = (TextView)findViewById(R.id.textlight);
-        valuelight = (TextView)findViewById(R.id.valuelight);
-        type = (TextView)findViewById(R.id.type);
-        x = (TextView)findViewById(R.id.rx);
-        y = (TextView)findViewById(R.id.ry);
-        z = (TextView)findViewById(R.id.rz);
-        gx = (TextView)findViewById(R.id.gx);
+        existlight = (TextView) findViewById(R.id.textlight);
+        valuelight = (TextView) findViewById(R.id.valuelight);
+        type = (TextView) findViewById(R.id.type);
+        x = (TextView) findViewById(R.id.rx);
+        y = (TextView) findViewById(R.id.ry);
+        z = (TextView) findViewById(R.id.rz);
+/*      gx = (TextView)findViewById(R.id.gx);
         gy = (TextView)findViewById(R.id.gy);
-        gz = (TextView)findViewById(R.id.gz);
-        record = (TextView)findViewById(R.id.record);
+        gz = (TextView)findViewById(R.id.gz);*/
+        speed = (TextView)findViewById(R.id.kecepatan );
+        record = (TextView) findViewById(R.id.record);
 
-        sensorManager = (SensorManager)this.getSystemService(SENSOR_SERVICE);
+        //sensor
+        sensorManager = (SensorManager) this.getSystemService(SENSOR_SERVICE);
         lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY); //TYPE_LIGHT
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        gyrometer = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        //gyrometer = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
-        String Mytemp = (lightSensor!=null && gyrometer!=null) ? "Available Proximity & Gyro Sensor" : "Unavailable Proximity & Gyro Sensor";
+        //speed
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        String Mytemp = (lightSensor != null && gyrometer != null) ? "Available Proximity" : "Unavailable Proximity & Gyro Sensor";
         existlight.setText(Mytemp);
 
         sensorEventListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
-                if(event.sensor.getType() == Sensor.TYPE_PROXIMITY){
-                    valuelight.setText(""+event.values[0]);
+                if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+                    valuelight.setText("" + event.values[0]);
                     myvaluelight = event.values[0];
-                }
-                else if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-                    if(myvaluelight==0) {
+                } else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                    if (myvaluelight == 0) {
                         isrecord = false;
-                        datefile = (datefile.isEmpty() && datefile!=null) ? new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()) : datefile;
+                        datefile = (datefile.isEmpty() && datefile != null) ? new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()) : datefile;
                         record.setText("Recording Data");
-                        x.setText("x : "+event.values[0]);
-                        y.setText("y : "+event.values[1]);
-                        z.setText("z : "+event.values[2]);
+                        x.setText("x : " + event.values[0]);
+                        y.setText("y : " + event.values[1]);
+                        z.setText("z : " + event.values[2]);
                         nowtime = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss").format(new Date());
-                        /*dataAcce.add(new String[]{
-                                    nowtime,
-                                    Float.toString(event.values[0]),
-                                    Float.toString(event.values[1]),
-                                    Float.toString(event.values[2])
-                                });*/
-                        if(counter<50){
+                        dataAcce.add(new String[]{
+                                nowtime,
+                                Float.toString(event.values[0]),
+                                Float.toString(event.values[1]),
+                                Float.toString(event.values[2])
+                        });
+                       /* if(counter<50){
                             dataTest[counter][0] = event.values[0];
                             dataTest[counter][1] = event.values[1];
                             dataTest[counter][2] = event.values[2];
                             counter++;
-                        }
-                        if(counter==50){
+                        }*/
+                        /*if(counter==50){
                             float[] temp = {0,0,0};
                             for(int i=0;i<50;i++){
                                 temp[0] += dataTest[i][0]/50;
@@ -229,10 +239,9 @@ public class StartProgram extends AppCompatActivity{
                             String tempOn = (sum[0]<sum[1]) ? "Sedang Naik Motor" : "Tidak Naik Motor" ;
                             type.setText(tempOn);
                             counter = 0;
-                        }
+                        }*/
 
-                    }
-                    else{
+                    } else {
                         isrecord = true;
                         record.setText("Not Record Data");
                         x.setText("X : -");
@@ -240,7 +249,7 @@ public class StartProgram extends AppCompatActivity{
                         z.setText("Z : -");
                     }
 
-                    if(isrecord){
+                    if (isrecord) {
                         /*try {
                             writeTocsv();
                         } catch (IOException e) {
@@ -249,8 +258,10 @@ public class StartProgram extends AppCompatActivity{
                         datefile = "";
                         dataAcce.clear();
                     }
+                } else if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+
                 }
-                else if(event.sensor.getType() == Sensor.TYPE_GYROSCOPE){
+                /*else if(event.sensor.getType() == Sensor.TYPE_GYROSCOPE){
                     if(myvaluelight==0){
                         isrecordGyro = false;
                         datefilegyro = (datefilegyro.isEmpty() && datefilegyro!=null) ? new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()) : datefilegyro;
@@ -258,12 +269,12 @@ public class StartProgram extends AppCompatActivity{
                         gy.setText("y : "+event.values[1]);
                         gz.setText("z : "+event.values[2]);
                         nowtime = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss").format(new Date());
-                        /*dataGyro.add(new String[]{
+                        *//*dataGyro.add(new String[]{
                                 nowtime,
                                 Float.toString(event.values[0]),
                                 Float.toString(event.values[1]),
                                 Float.toString(event.values[2])
-                        });*/
+                        });*//*
                     }
                     else{
                         isrecordGyro = true;
@@ -272,15 +283,15 @@ public class StartProgram extends AppCompatActivity{
                         gz.setText("X : -");
                     }
                     if(isrecordGyro){
-                        /*try{
+                        *//*try{
                             writeTocsvGyro();
                         }catch (IOException e) {
                             e.printStackTrace();
-                        }*/
+                        }*//*
                         datefilegyro = "";
                         dataGyro.clear();
                     }
-                }
+                }*/
             }
 
             @Override
@@ -288,8 +299,46 @@ public class StartProgram extends AppCompatActivity{
 
             }
         };
-        sensorManager.registerListener(sensorEventListener,lightSensor,SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(sensorEventListener,accelerometer,SensorManager.SENSOR_DELAY_GAME);
-        sensorManager.registerListener(sensorEventListener,gyrometer,SensorManager.SENSOR_DELAY_NORMAL);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                location.getLatitude();
+                speed.setText("Kecepatan : "+location.getSpeed());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        //sensor
+        sensorManager.registerListener(sensorEventListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(sensorEventListener, gyrometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //speed
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
 }
